a1a830ccd58882afbe9514936fe825dc050ba898
mf1228 <98032641+mf1228@users.noreply.github.com>
2022-01-19 19:42:48 +0530

    Add files via upload

---
+++ mohd FAiz.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+cars=['benz','audi','bmw','kia','hyundari']
+print(cars)
+print(sorted(cars))
+
+
+# In[6]:
+
+
+cars.sort()
+print(cars)
+
+
+# In[10]:
+
+
+cars.reverse()
+print(cars)
+
+
+# In[11]:
+
+
+len(cars)
+
+
+# In[15]:
+
+
+student=['ravi','abdul','raja','faisl','pk','mz','fz']
+print(student[0:2])
+print(student[0:8:3])
+print(student[0:8:4])
+print(student[0:8:2])
+
+
+# In[20]:
+
+
+print(f"keep up the good work,{student[0].title()}")
+
+
+# In[23]:
+
+
+for x in student:
+    print(f"keep up the good work,{x.title()}")
+    print(f"goog luck,{x.title()}\n")
+print("thank you ")    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
